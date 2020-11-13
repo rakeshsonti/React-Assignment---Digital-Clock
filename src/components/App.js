e02c621d55bcccb6585d7280e50d30f0143e5f69
Rambhajan Sonti <rambhajansonti>
2020-11-13 11:22:53 +0530

    2nd commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,51 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
-  getPadding(val) {
+constructor(props)
+{
+    super(props);
+    this.state={
+        time:new Date(),
+    };
+    this.setIntervalId=null;
+}
+
+    getPadNumberToTwoDigits(val) {
     let newVar;
-    val < 10 ? (newVar = `0${val}`) : (newVar = val);
+    newVar=(val < 10?`0`:'')+val;
     return newVar;
   }
   getTimeString() {
-    const time = new Date();
-    const currHour = time.getHours();
-    const currMinutes = this.getPadding(time.getMinutes());
-    const currSeconds = this.getPadding(time.getSeconds());
-    const amorpm = currHour > 12 ? `PM` : "AM";
-    const newHour = currHour > 12 ? currHour - 12 : currHour;
+    const currTime = this.state.time;
+    const [currHour,currMinutes,currSeconds,amorpm]=
+   [
+    currTime.getHours(),
+    this.getPadNumberToTwoDigits(currTime.getMinutes()),
+    this.getPadNumberToTwoDigits(currTime.getSeconds()),
+    currHour >= 12 ? `PM` : "AM",
+   ]
+   const newHour = currHour > 12 ? currHour - 12 : currHour;
     // console.log(newHour, currMinutes, currSeconds, amorpm);
-    return `${newHour}:${currMinutes}:${currSeconds} ${amorpm}`;
+    const finalString= `${newHour}:${currMinutes}:${currSeconds} ${amorpm}`;
+    return finalString;
+  }
+  componentWillUnmount()
+  {
+      clearInterval(this.setIntervalId);
+  }
+  componentDidMount()
+  {
+      this.setIntervalId=setInterval(()=>{
+          this.setState({
+              time:new Date()
+            })
+      },1*1000)
   }
   render() {
     return (
       <>
         <div className="Clock">
+
           <h3 id="time">{this.getTimeString()}</h3>
         </div>
       </>
